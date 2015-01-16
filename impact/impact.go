a82575e434afe7906988d3094048b17fb3149e1a
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2015-01-16 16:26:41 +1300

    no-op based processing.

--- impact/impact.go
+++ impact/impact.go
@@ -48,23 +48,23 @@ func (i Intensity) SetErr(err error) {
 	i.err = err
 }
 
-// Valid returns true if the Intensity pointed to by i is valid, false if not.
-// For invalid intensity additional information is available in i.Err().
+// Valid sets Err if i is invalid.
 // i.Comment is trimmed to 140 char.
-func (i *Intensity) Valid() bool {
+func (i *Intensity) Valid() {
+	if i.err != nil {
+		return
+	}
+
 	if !source.MatchString(i.Source) {
 		i.err = fmt.Errorf("invalid source: %s must match %s", i.Source, source.String())
-		return false
 	}
 
 	if !(i.Quality == "measured" || i.Quality == "reported") {
 		i.err = fmt.Errorf("invalid quality: %s", i.Quality)
-		return false
 	}
 
 	if i.MMI < 1 || i.MMI > 12 {
 		i.err = fmt.Errorf("invalid MMI: %d", i.MMI)
-		return false
 	}
 
 	// we currently use postgis and it will convert any numeric value to valid long lat so this
@@ -72,42 +72,36 @@ func (i *Intensity) Valid() bool {
 	// error messages.
 	if !(i.Longitude < 180.0 && i.Longitude > -180.0) {
 		i.err = fmt.Errorf("longitude not in range -180 to 180: %f", i.Longitude)
-		return false
 	}
 
 	if !(i.Latitude < 90.0 && i.Latitude > -90.0) {
 		i.err = fmt.Errorf("latitude not in range -90 to 90: %f", i.Latitude)
-		return false
 	}
 
 	if len(i.Comment) > 139 {
 		i.Comment = i.Comment[0:139]
 	}
 
-	return true
+	return
 }
 
-// Old returns true if the intensity pointed to by i is older then 60 minutes.
-// When Old returns true additional information is available in i.Err()
-func (i *Intensity) Old() bool {
+// Old sets Err if the intensity pointed to by i is older then 60 minutes.
+func (i *Intensity) Old() {
+	if i.err != nil {
+		return
+	}
 	// No disctinction between measured and reported intensity at the moment.
 	// We may need to allow slightly older reported intensity messages in the future?
 	if i.Time.Before(time.Now().UTC().Add(MeasuredAge)) {
 		i.err = fmt.Errorf("old message for %s", i.Source)
-		return true
 	}
-	return false
+	return
 }
 
-// Marshal returns the JSON encoding of i.
-func (i *Intensity) Marshal() ([]byte, error) {
-	b, err := json.Marshal(i)
-	i.err = err
-	return b, err
-}
-
-// Unmarshal unmarshals the JSON in b into i.
-func (i *Intensity) Unmarshal(b []byte) error {
+func (i *Intensity) Decode(b []byte) {
 	i.err = json.Unmarshal(b, i)
-	return i.err
+}
+
+func (i *Intensity) Encode() ([]byte, error) {
+	return json.Marshal(i)
 }
--- msg.go
+++ msg.go
@@ -5,8 +5,7 @@
 // There is no requirement for a flow to have both send and receive.
 //
 // Message processing is tracked into expvar.  The following values are added:
-//  * messages.inbound - the number of messages inbound for processing in 20s.  Updated every 20s.
-//  * messages.outbound - the number of messages outbound from processing in 20s.  Updated every 20s.
+//  * messages.process - the number of messages sent to process.  Updated every 20s.
 //  * messages.error - the number of messages that resulted in an error in 20s.  Updates every 20s.
 //  * messages.time - the average processing time per message in 20s.  Updates every 20s.
 //
@@ -20,38 +19,37 @@ import (
 )
 
 var (
-	pt  metrics.Timer
-	in  metrics.Rate
-	out metrics.Rate
-	e   metrics.Rate
-	v   = expvar.NewMap("messages")
+	pt metrics.Timer
+	p  metrics.Rate
+	e  metrics.Rate
+	v  = expvar.NewMap("messages")
 )
 
 func init() {
 	pt.Init(time.Duration(20) * time.Second)
-	in.Init(time.Duration(20)*time.Second, time.Duration(20)*time.Second)
-	out.Init(time.Duration(20)*time.Second, time.Duration(20)*time.Second)
+	p.Init(time.Duration(20)*time.Second, time.Duration(20)*time.Second)
 	e.Init(time.Duration(20)*time.Second, time.Duration(20)*time.Second)
 	v.Init()
 	v.Set("time", &pt)
-	v.Set("inbound", &in)   // use 'inbound' instead of received incase we ever want to track that as well.
-	v.Set("outbound", &out) // use 'outbound' instead of sent incase we ever want to track that as well.
+	v.Set("process", &p)
 	v.Set("error", &e)
 }
 
+// Message defines an interface the allows for message processing.
+// Types that implement Message should use a no-op approach in Process()
+// based on checking Err() (or it's private val).  See for example
+// github.com/GeoNet/msg/impact.Intensity
 type Message interface {
-	Unmarshal(b []byte) error
-	Marshal() ([]byte, error)
-	// Process returns a bool that can be used as a hint for the
-	// transport.  If false it may be appropriate for the transport
-	// to try to redeliver the message.
-	Process() bool
+	Decode(b []byte)
+	Encode() ([]byte, error)
+	Process() (reprocess bool) // a hint to try to reprocess the message.
 	Err() error
 }
 
 // Process excutes m.Process with logging and metrics.
 // Logs errors.  Increments metrics.
 func Process(m Message) bool {
+	p.Inc()
 	start := time.Now()
 	s := m.Process()
 	pt.Inc(start)
@@ -60,34 +58,4 @@ func Process(m Message) bool {
 		e.Inc()
 	}
 	return s
-}
-
-// Decode uses m.Unmarshal to decode an inbound message.  Returns false
-// if the message cannot be unmarshaled in which case
-// it may be appropriate for the transport to discard the message.
-// Logs errors.  Increments metrics.
-func Decode(m Message, b []byte) bool {
-	in.Inc()
-	if err := m.Unmarshal(b); err != nil {
-		log.Printf("WARN: %s", err)
-		e.Inc()
-		return false
-	}
-	return true
-}
-
-// Encode uses m.Marshal to encode m into the byte slice pointed to by b.
-// Returns false if the message cannot be marshaled in which case it is
-// probably inappropriate to send the message to the outbound transport.
-// Logs errors.  Increments metrics.
-func Encode(m Message, b *[]byte) bool {
-	out.Inc()
-	var err error
-	*b, err = m.Marshal()
-	if err != nil {
-		log.Printf("WARN: %s", err)
-		e.Inc()
-		return false
-	}
-	return true
 }
