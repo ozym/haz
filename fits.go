45b6519238b21eaac64fa36a2985e780b46e92b9
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2015-05-19 14:57:53 +1200

    Fits message.

---
+++ fits.go
@@ -0,0 +1,40 @@
+package msg
+
+import (
+	"encoding/json"
+	"time"
+)
+
+/*
+Observation is a useful wire format for Fits data.
+*/
+type Observation struct {
+	NetworkID string
+	SiteID    string
+	TypeID    string
+	MethodID  string
+	DateTime  time.Time
+	Value     float64
+	Error     float64 // Error for the observation value.  0.0 for no or unknown error.
+	err       error
+}
+
+func (o *Observation) SetErr(err error) {
+	o.err = err
+}
+
+func (o *Observation) Err() error {
+	return o.err
+}
+
+func (o *Observation) Decode(b []byte) {
+	o.err = json.Unmarshal(b, o)
+}
+
+func (o *Observation) Encode() ([]byte, error) {
+	if o.err != nil {
+		return nil, o.err
+	}
+
+	return json.Marshal(o)
+}
