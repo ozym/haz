a8470edeaaa1b86b56ddde1ff4a3aa1bc3960919
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2015-07-25 02:58:50 +1200

    Import impact consumer.

---
+++ impact-intensity-consumer/README.md
@@ -0,0 +1,45 @@
+# Impact Intensity Consumer
+
+Listens to an SQS queue for intensity messages and saves them to the impact DB.
+
+[![Build Status](https://snap-ci.com/GeoNet/impact-intensity-consumer/branch/master/build_image)](https://snap-ci.com/GeoNet/impact-intensity-consumer/branch/master)
+[![Docker Repository on Quay.io](https://quay.io/repository/geonet/impact-intensity-consumer/status "Docker Repository on Quay.io")](https://quay.io/repository/geonet/impact-intensity-consumer)
+
+## Message Format
+
+## Deployment
+
+### AWS Resources
+
+There is an AWS Cloudformation template in the cloudformation directory to create the SQS queue and a user to access it.  Use the ouput of this template for the configuration in impact-intensity-consumer.json.  Subscribe a suitable endpoint to the alarm topic for the stack.
+
+### Properties 
+
+Either or both of: 
+1. Copy an appropriately edited version of `impact-intensity-consumer.json` to `/etc/sysconfig/impact-intensity-consumer.json`  This should include write access credentials for accessing the impact database.
+2. Refer to docker-run.sh for overriding from env var.
+
+### Scaling
+
+The number of concurrent SQS listeners can be can be controlled with the config parameter `NumberOfListeners`.  
+
+### Monitoring
+
+Metrics are sent to Librato Metrics.
+
+### Logging
+
+Logs to standard out.
+
+### Database Fault Tolerance.
+
+The app is tolerant of (most) database faults:
+
+1. Won't fully start until the DB can be contacted.
+2. Blocks processing if a message can't be stored in the DB.
+
+Case 2. can be caused by the DB being uncontactable for a while in which case the app will block message processing until the database becomes available again and then recover.  If it is caused by schema or permission changes then app will block message processing until the problem is fixed.
+
+### AWS SQS Fault Tolerance
+
+The app is tolerant of networking faults for SQS.  This does mean that it will not exit for SQS config errors and will loop logging ERROR messages.
---
+++ impact-intensity-consumer/impact-intensity-consumer.go
@@ -0,0 +1,137 @@
+package main
+
+// impact-intensity-consumer to take intensity messages from SQS and store them in the impact DB.
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/GeoNet/cfg"
+	"github.com/GeoNet/haz/msg"
+	"github.com/GeoNet/haz/sqs"
+	"github.com/GeoNet/log/logentries"
+	_ "github.com/lib/pq"
+	"log"
+	"time"
+)
+
+//go:generate configer impact-intensity-consumer.json
+var (
+	config         = cfg.Load()
+	db             *sql.DB
+	retry          = time.Duration(30) * time.Second
+	expireInterval = time.Duration(10) * time.Second
+)
+
+type message struct {
+	msg.Intensity
+}
+
+func init() {
+	logentries.Init(config.Logentries.Token)
+	msg.InitLibrato(config.Librato.User, config.Librato.Key, config.Librato.Source)
+	config.SQS.MaxNumberOfMessages = 1
+	config.SQS.VisibilityTimeout = 600
+	config.SQS.WaitTimeSeconds = 20
+}
+
+// main sets up the DB connection and then runs listen to process intensity messages from SQS.
+func main() {
+	var err error
+
+	db, err = sql.Open("postgres", config.DataBase.Postgres())
+	if err != nil {
+		log.Fatalf("ERROR: problem with DB config: %s", err)
+	}
+	defer db.Close()
+
+	db.SetMaxIdleConns(config.DataBase.MaxIdleConns)
+	db.SetMaxOpenConns(config.DataBase.MaxOpenConns)
+
+	dbPing()
+	go deleteExpired()
+
+	log.Println("starting message listener.")
+	listen()
+}
+
+// dbPing does not return until it has successfully pinged the DB.
+func dbPing() {
+	for {
+		if err := db.Ping(); err != nil {
+			log.Printf("WARN - pinging DB: %s", err)
+			log.Println("WARN - waiting then trying DB again.")
+			time.Sleep(retry)
+			continue
+		}
+		break
+	}
+}
+
+// listen listens to the SQS queue for intensity messages and saves them to the DB.
+func listen() {
+	rx, dx, err := sqs.InitRx(config.SQS)
+	if err != nil {
+		log.Fatalf("ERROR - problem creating SQS from config: %s", err)
+	}
+
+	for {
+		r := <-rx
+		m := message{}
+		m.Decode([]byte(r.Body))
+		if !msg.Process(&m) {
+			dx <- r.ReceiptHandle
+		}
+	}
+}
+
+func (m *message) Process() bool {
+	m.Valid()
+	m.Old()
+
+	if m.Err() != nil {
+		return false
+	}
+
+	switch m.Quality {
+	case "measured":
+		m.saveMeasured()
+	case "reported":
+		m.saveReported()
+	default:
+		m.SetErr(fmt.Errorf("no method to save intensity message with quality: %s", m.Quality))
+		return false
+	}
+
+	if m.Err() != nil {
+		dbPing()
+		return true
+	}
+
+	return false
+}
+
+func (m *message) saveMeasured() {
+	_, err := db.Exec("select impact.add_intensity_measured($1, $2, $3, $4, $5)", m.Source, m.Longitude, m.Latitude, m.Time, m.MMI)
+
+	if err != nil {
+		m.SetErr(err)
+	}
+}
+
+func (m *message) saveReported() {
+	_, err := db.Exec("select impact.add_intensity_reported($1, $2, $3, $4, $5, $6)", m.Source, m.Longitude, m.Latitude, m.Time, m.MMI, m.Comment)
+
+	if err != nil {
+		m.SetErr(err)
+	}
+}
+
+func deleteExpired() {
+	for {
+		_, err := db.Exec("delete from impact.intensity_measured where time < $1", time.Now().UTC().Add(msg.MeasuredAge))
+		if err != nil {
+			log.Printf("WARN expiring measured instensity values: %s", err)
+		}
+		time.Sleep(expireInterval)
+	}
+}
