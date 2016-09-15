1710d8d7e6b9647892d558322b3bf10a86b577be
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2016-09-15 18:14:22 +1200

    Gofmt only.

--- sc3ml-to-quakeml/handlers_auto.go
+++ sc3ml-to-quakeml/handlers_auto.go
@@ -4,98 +4,96 @@ package main
 // It was created with weftgenapi from github.com/GeoNet/weft/weftgenapi
 
 import (
-"bytes"
-"github.com/GeoNet/weft"
-"net/http"
-"io/ioutil"
+	"bytes"
+	"github.com/GeoNet/weft"
+	"io/ioutil"
+	"net/http"
 )
 
 var mux = http.NewServeMux()
 
-
 func init() {
-mux.HandleFunc("/api-docs", weft.MakeHandlerPage(docHandler))
-mux.HandleFunc("/csv/1.0.0/", weft.MakeHandlerAPI(csv1_0_0sHandler))
-mux.HandleFunc("/quakeml/1.2/", weft.MakeHandlerAPI(quakeml1_2sHandler))
-mux.HandleFunc("/quakeml-rt/1.2/", weft.MakeHandlerAPI(quakeml_rt1_2sHandler))
+	mux.HandleFunc("/api-docs", weft.MakeHandlerPage(docHandler))
+	mux.HandleFunc("/csv/1.0.0/", weft.MakeHandlerAPI(csv1_0_0sHandler))
+	mux.HandleFunc("/quakeml/1.2/", weft.MakeHandlerAPI(quakeml1_2sHandler))
+	mux.HandleFunc("/quakeml-rt/1.2/", weft.MakeHandlerAPI(quakeml_rt1_2sHandler))
 }
 
 func docHandler(r *http.Request, h http.Header, b *bytes.Buffer) *weft.Result {
-switch r.Method {
-case "GET":
-by, err := ioutil.ReadFile("assets/api-docs/index.html")
-if err != nil {
-return weft.InternalServerError(err)
-}
-b.Write(by)
-return &weft.StatusOK
-default:
-return &weft.MethodNotAllowed
-}
+	switch r.Method {
+	case "GET":
+		by, err := ioutil.ReadFile("assets/api-docs/index.html")
+		if err != nil {
+			return weft.InternalServerError(err)
+		}
+		b.Write(by)
+		return &weft.StatusOK
+	default:
+		return &weft.MethodNotAllowed
+	}
 }
 func csv1_0_0sHandler(r *http.Request, h http.Header, b *bytes.Buffer) *weft.Result {
-switch r.Method {
-case "GET":
-switch r.Header.Get("Accept") {
-case "text/csv":
-if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
-return res
-}
-h.Set("Content-Type", "text/csv")
-return csv(r, h, b)
-default:
-if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
-return res
-}
-h.Set("Content-Type", "text/csv")
-return csv(r, h, b)
-}
-default:
-return &weft.MethodNotAllowed
-}
+	switch r.Method {
+	case "GET":
+		switch r.Header.Get("Accept") {
+		case "text/csv":
+			if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
+				return res
+			}
+			h.Set("Content-Type", "text/csv")
+			return csv(r, h, b)
+		default:
+			if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
+				return res
+			}
+			h.Set("Content-Type", "text/csv")
+			return csv(r, h, b)
+		}
+	default:
+		return &weft.MethodNotAllowed
+	}
 }
 
 func quakeml1_2sHandler(r *http.Request, h http.Header, b *bytes.Buffer) *weft.Result {
-switch r.Method {
-case "GET":
-switch r.Header.Get("Accept") {
-case "text/xml":
-if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
-return res
-}
-h.Set("Content-Type", "text/xml")
-return quakeml12(r, h, b)
-default:
-if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
-return res
-}
-h.Set("Content-Type", "text/xml")
-return quakeml12(r, h, b)
-}
-default:
-return &weft.MethodNotAllowed
-}
+	switch r.Method {
+	case "GET":
+		switch r.Header.Get("Accept") {
+		case "text/xml":
+			if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
+				return res
+			}
+			h.Set("Content-Type", "text/xml")
+			return quakeml12(r, h, b)
+		default:
+			if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
+				return res
+			}
+			h.Set("Content-Type", "text/xml")
+			return quakeml12(r, h, b)
+		}
+	default:
+		return &weft.MethodNotAllowed
+	}
 }
 
 func quakeml_rt1_2sHandler(r *http.Request, h http.Header, b *bytes.Buffer) *weft.Result {
-switch r.Method {
-case "GET":
-switch r.Header.Get("Accept") {
-case "text/xml":
-if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
-return res
+	switch r.Method {
+	case "GET":
+		switch r.Header.Get("Accept") {
+		case "text/xml":
+			if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
+				return res
+			}
+			h.Set("Content-Type", "text/xml")
+			return quakeml12RT(r, h, b)
+		default:
+			if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
+				return res
+			}
+			h.Set("Content-Type", "text/xml")
+			return quakeml12RT(r, h, b)
+		}
+	default:
+		return &weft.MethodNotAllowed
+	}
 }
-h.Set("Content-Type", "text/xml")
-return quakeml12RT(r, h, b)
-default:
-if res := weft.CheckQuery(r, []string{}, []string{}); !res.Ok {
-return res
-}
-h.Set("Content-Type", "text/xml")
-return quakeml12RT(r, h, b)
-}
-default:
-return &weft.MethodNotAllowed
-}
-}
-
